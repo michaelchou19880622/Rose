ff7c7c2afefaf890fdd432c826b9050d33a189c1
mopack-XtremeApp <mopack@xtremeapp.com.tw>
2019-08-12 16:03:24 +0800

    get rid of LinePointSendGroup

--- com.bcs.core.linepoint/src/main/java/com/bcs/core/linepoint/db/repository/LinePointSendGroupQueryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bcs.core.linepoint.db.repository;
-
-import com.bcs.core.db.persistence.EntityRepository;
-import com.bcs.core.linepoint.db.entity.LinePointSendGroupQuery;
-
-public interface LinePointSendGroupQueryRepository extends EntityRepository<LinePointSendGroupQuery, String>{
-
-}

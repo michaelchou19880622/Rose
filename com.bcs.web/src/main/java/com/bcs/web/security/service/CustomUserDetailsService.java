e3329064cfc8f907905eb93785f25127e99dab2c
Alan Chen <t0239184ps@gmail.com>
2019-09-11 11:16:09 +0800

    [UPDATE] 增加登入相關註解及Log

--- com.bcs.web/src/main/java/com/bcs/web/security/service/CustomUserDetailsService.java
+++ com.bcs.web/src/main/java/com/bcs/web/security/service/CustomUserDetailsService.java
@@ -1,39 +1,36 @@
 package com.bcs.web.security.service;
 
+import com.bcs.core.db.entity.AdminUser;
+import com.bcs.core.db.service.AdminUserService;
+import com.bcs.core.web.security.CustomUser;
 import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-import com.bcs.core.db.entity.AdminUser;
-import com.bcs.core.db.service.AdminUserService;
-import com.bcs.core.web.security.CustomUser;
-
 /**
  * Spring Security 查詢登入者 UserDetails 的 Service
- * 
+ *
  * @author Kevin
- * 
  */
 public class CustomUserDetailsService implements UserDetailsService {
 
-	private static Logger logger = Logger
-			.getLogger(CustomUserDetailsService.class);
+    private static Logger logger = Logger.getLogger(CustomUserDetailsService.class);
 
-	@Autowired
-	private AdminUserService adminUserService;
+    @Autowired
+    private AdminUserService adminUserService;
 
-	@Override
-	public UserDetails loadUserByUsername(String account)
-			throws UsernameNotFoundException {
-		AdminUser adminUser = adminUserService.findByAccountIgnoreCase(account);
+    @Override
+    public UserDetails loadUserByUsername(String account) throws UsernameNotFoundException {
+        logger.info("account: " + account);
+        AdminUser adminUser = adminUserService.findByAccountIgnoreCase(account);
 
-		if (adminUser == null) {
-			logger.info("User not found");
-			throw new UsernameNotFoundException("Account not found");
-		}
-
-		return new CustomUser(adminUser);
-	}
+        if (adminUser == null) {
+            logger.info("User not found");
+            throw new UsernameNotFoundException("Account not found");
+        }
+        logger.info("Return New CustomUser!!");
+        return new CustomUser(adminUser);
+    }
 }

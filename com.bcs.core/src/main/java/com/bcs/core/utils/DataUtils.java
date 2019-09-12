21093b61fc795961224eafd62271b2897962a1e8
Alan Chen <t0239184ps@gmail.com>
2019-09-12 18:16:03 +0800

    [UPDATE] 更新工具類別

--- com.bcs.core/src/main/java/com/bcs/core/utils/DataUtils.java
+++ com.bcs.core/src/main/java/com/bcs/core/utils/DataUtils.java
@@ -1,5 +1,8 @@
 package com.bcs.core.utils;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.google.gson.GsonBuilder;
 import lombok.experimental.UtilityClass;
 
@@ -19,5 +22,15 @@ public class DataUtils {
 
     public static String formatDateToString(Date date, String format){
         return new SimpleDateFormat(format).format(date);
+    }
+
+    public static String toPrettyJsonUseJackson(Object obj) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            return obj.getClass().getName() + '@' + Integer.toHexString(obj.hashCode());
+        }
     }
 }
--- com.bcs.core/src/main/java/com/bcs/core/utils/ObjectUtil.java
+++ com.bcs.core/src/main/java/com/bcs/core/utils/ObjectUtil.java
@@ -7,55 +7,64 @@ import java.util.List;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class ObjectUtil {
 
-	public static String listStringToString(List<String> list){
-		if(list != null && list.size() > 0){
-			if(list.size() == 1){
-				return list.get(0);
-			}
-			
-			String result = list.get(0);
-			for(int i = 1; i < list.size(); i++){
-				result += "," + list.get(i);
-			}
-			return result;
-		}
-		return null;
-	}
+    public static String listStringToString(List<String> list) {
+        if (list != null && list.size() > 0) {
+            if (list.size() == 1) {
+                return list.get(0);
+            }
 
-	public static String listObjectToString(List<Object> list){
-		List<String> s = new ArrayList<String>();
-		for(Object o : list){
-			s.add(ObjectUtil.objectToJsonStr(o));
-		}
-		return ObjectUtil.listStringToString(s);
-	}
+            String result = list.get(0);
+            for (int i = 1; i < list.size(); i++) {
+                result += "," + list.get(i);
+            }
+            return result;
+        }
+        return null;
+    }
 
-	public static String objectToJsonStr(Object content) {
-	    try {
-	    	return (new ObjectMapper()).writeValueAsString(content);
-		} catch (JsonProcessingException e) {
-			return content.getClass().getName() + '@' + Integer.toHexString(content.hashCode());
-		}
-	}
+    public static String listObjectToString(List<Object> list) {
+        List<String> s = new ArrayList<>();
+        for (Object o : list) {
+            s.add(ObjectUtil.objectToJsonStr(o));
+        }
+        return ObjectUtil.listStringToString(s);
+    }
 
-	public static ObjectNode jsonStrToObjectNode(String jsonStr) throws JsonProcessingException, IOException {
-		return (ObjectNode)(new ObjectMapper()).readTree(jsonStr);
-	}
+    /**
+     * Converter Object To Pretty Json Use Jackson
+     * @param obj Object
+     * @return Pretty Json
+     */
+    public static String objectToJsonStr(Object obj) {
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            mapper.enable(SerializationFeature.INDENT_OUTPUT);
+            return mapper.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            return obj.getClass().getName() + '@' + Integer.toHexString(obj.hashCode());
+        }
+    }
 
-	public static ArrayNode jsonStrToArrayNode(String jsonStr) throws JsonProcessingException, IOException {
-		return (ArrayNode)(new ObjectMapper()).readTree(jsonStr);
-	}
 
-	public static JsonNode jsonStrToJsonNode(String jsonStr) throws JsonProcessingException, IOException {
-		return new ObjectMapper().readTree(jsonStr);
-	}
+    public static ObjectNode jsonStrToObjectNode(String jsonStr) throws JsonProcessingException, IOException {
+        return (ObjectNode) (new ObjectMapper()).readTree(jsonStr);
+    }
 
-	public static <T> T jsonStrToObject(String jsonStr, Class<T> targetClass) throws JsonProcessingException, IOException {
-		return new ObjectMapper().readValue(jsonStr, targetClass);
-	}
+    public static ArrayNode jsonStrToArrayNode(String jsonStr) throws JsonProcessingException, IOException {
+        return (ArrayNode) (new ObjectMapper()).readTree(jsonStr);
+    }
+
+    public static JsonNode jsonStrToJsonNode(String jsonStr) throws JsonProcessingException, IOException {
+        return new ObjectMapper().readTree(jsonStr);
+    }
+
+    public static <T> T jsonStrToObject(String jsonStr, Class<T> targetClass) throws JsonProcessingException, IOException {
+        return new ObjectMapper().readValue(jsonStr, targetClass);
+    }
 }

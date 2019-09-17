3674b3db2344d9d8e0b19f615a0ddc39747d76c3
Alan Chen <t0239184ps@gmail.com>
2019-09-17 19:25:50 +0800

    [ADD] 新增報表通用Builder及Service

---
+++ com.bcs.core/src/main/java/com/bcs/core/report/service/ExportService.java
@@ -0,0 +1,35 @@
+package com.bcs.core.report.service;
+
+import com.bcs.core.report.builder.ExportExcelBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.util.IOUtils;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+@Slf4j
+public class ExportService {
+    public void exportExcel(HttpServletResponse response, ExportExcelBuilder builder){
+        builder.export();
+        try (
+                InputStream inp = new FileInputStream(builder.getOutPutPath() + builder.getOutputFileName())
+        ){
+            response.setContentType("application/download; charset=UTF-8");
+            response.setHeader("Content-Disposition", "attachment; filename=" + builder.getOutputFileName());
+            response.setCharacterEncoding("UTF-8");
+            OutputStream outputStream = response.getOutputStream();
+            log.info("[loadFileToResponse]");
+            IOUtils.copy(inp, outputStream);
+
+            response.flushBuffer();
+            if (outputStream != null) {
+                outputStream.close();
+            }
+        } catch (IOException e) {
+            log.error("Exception");
+        }
+    }
+}

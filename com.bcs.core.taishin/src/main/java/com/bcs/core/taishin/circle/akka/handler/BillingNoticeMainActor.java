4e51089616fff4353e852cb58c8e704faa98ab1a
Alan Chen <t0239184ps@gmail.com>
2020-01-10 10:34:12 +0800

    [UPDATE] Refactor Code

--- com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/akka/handler/BillingNoticeMainActor.java
+++ com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/akka/handler/BillingNoticeMainActor.java
@@ -3,17 +3,22 @@ package com.bcs.core.taishin.circle.akka.handler;
 import akka.actor.ActorRef;
 import akka.actor.UntypedActor;
 import com.bcs.core.spring.ApplicationContextProvider;
-import com.bcs.core.taishin.circle.db.entity.BillingNoticeContentTemplateMsg;
 import com.bcs.core.taishin.circle.db.entity.BillingNoticeDetail;
 import com.bcs.core.taishin.circle.db.entity.BillingNoticeMain;
 import com.bcs.core.taishin.circle.service.BillingNoticeService;
 import com.bcs.core.utils.AkkaRouterFactory;
+import com.bcs.core.utils.DataUtils;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
 
+/**
+ * @author ???
+ * @see com.bcs.core.taishin.circle.service.BillingNoticeAkkaService
+ */
 @Slf4j
 public class BillingNoticeMainActor extends UntypedActor {
     private final ActorRef pushMessageRouterActor;
@@ -22,58 +27,78 @@ public class BillingNoticeMainActor extends UntypedActor {
     private final ActorRef curfewActor;
 
     public BillingNoticeMainActor() {
-        pushMessageRouterActor = new AkkaRouterFactory<BillingNoticePushMessageActor>(getContext(), BillingNoticePushMessageActor.class, true).routerActor;
-        updateStatusRouterActor = new AkkaRouterFactory<BillingNoticeUpdateStatusActor>(getContext(), BillingNoticeUpdateStatusActor.class, true).routerActor;
-        expireRouterActor = new AkkaRouterFactory<BillingNoticeExpireActor>(getContext(), BillingNoticeExpireActor.class, true).routerActor;
-        curfewActor = new AkkaRouterFactory<BillingNoticeCurfewActor>(getContext(), BillingNoticeCurfewActor.class, true).routerActor;
+        pushMessageRouterActor = new AkkaRouterFactory<>(getContext(), BillingNoticePushMessageActor.class, true).routerActor;
+        updateStatusRouterActor = new AkkaRouterFactory<>(getContext(), BillingNoticeUpdateStatusActor.class, true).routerActor;
+        expireRouterActor = new AkkaRouterFactory<>(getContext(), BillingNoticeExpireActor.class, true).routerActor;
+        curfewActor = new AkkaRouterFactory<>(getContext(), BillingNoticeCurfewActor.class, true).routerActor;
     }
 
     @Override
-    public void onReceive(Object object) {
+    public void onReceive(final Object object) {
         try {
             if (object instanceof BillingNoticeMain) {
-
-                BillingNoticeMain billingNoticeMain = (BillingNoticeMain) object;
-                BillingNoticeService billingNoticeService = ApplicationContextProvider.getApplicationContext().getBean(BillingNoticeService.class);
-                BillingNoticeContentTemplateMsg template = billingNoticeMain.getTemplate();
-                boolean iscurfew = billingNoticeService.isCurfew(template, Calendar.getInstance());
-                log.info("iscurfew1: " + iscurfew);
-
-                if (iscurfew) {  //宵禁中
-                    curfewActor.tell(billingNoticeMain, this.getSelf());
-                } else if (Calendar.getInstance().getTime().after(billingNoticeMain.getExpiryTime())) { // Expire
-                    // update status 為不推送 並寄信
-                    expireRouterActor.tell(billingNoticeMain, this.getSelf());
-                } else {
-                    Integer buffer = 100;
-                    //找出未發送以及重試BillingNoticeDetail
-                    List<BillingNoticeDetail> details = billingNoticeMain.getDetails();
-                    List<BillingNoticeDetail> partition = null;
-                    Integer arrayLength = details.size();
-                    Integer pointer = 0;
-
-                    log.info("detail1" + details.toString());
-
-                    while (pointer < arrayLength) {
-                        Integer counter = 0;
-                        partition = new ArrayList<>();
-
-                        for (; (counter < buffer) && (pointer < arrayLength); counter++, pointer++) {
-                            partition.add(details.get(pointer));
-                        }
-                        BillingNoticeMain billingNoticeMain_clone = (BillingNoticeMain) billingNoticeMain.clone();
-
-                        billingNoticeMain_clone.setDetails(partition);
-
-                        pushMessageRouterActor.tell(billingNoticeMain_clone, this.getSelf());
-                    }
-                }
-
-            } else if (object instanceof BillingNoticeDetail) { // update status
-                updateStatusRouterActor.tell(object, this.getSelf());
+                pushRoute((BillingNoticeMain) object);
+            }
+            if (object instanceof BillingNoticeDetail) {
+                toUpdateActor(object);
             }
         } catch (Exception e) {
             log.error("Exception", e);
         }
+    }
+
+    private void pushRoute(final BillingNoticeMain object) {
+        BillingNoticeService billingNoticeService = ApplicationContextProvider.getApplicationContext().getBean(BillingNoticeService.class);
+
+        if (billingNoticeService.isCurfew(object.getTemplate(), Calendar.getInstance())) {
+            toCurfewActor(object);
+        } else if (DataUtils.isPast(object.getExpiryTime())) {
+            toExpiredActor(object);
+        } else {
+            pushProcess(object);
+        }
+    }
+
+    private void pushProcess(BillingNoticeMain object) {
+        int buffer = 100;
+        List<BillingNoticeDetail> details = object.getDetails();
+        List<BillingNoticeDetail> partition;
+        int detailSize = details.size();
+        int count = 0;
+
+        log.info("Detail: {}", DataUtils.toPrettyJsonUseJackson(details));
+
+        while (count < detailSize) {
+            int counter = 0;
+            partition = new ArrayList<>();
+            for (; (counter < buffer) && (count < detailSize); counter++, count++) {
+                partition.add(details.get(count));
+            }
+            log.info("To Akka Count: {}, Partition Size: {}", count, partition.size());
+
+            BillingNoticeMain billingNoticeMainClone = ObjectUtils.clone(object);
+            billingNoticeMainClone.setDetails(partition);
+            toPushActor(billingNoticeMainClone);
+        }
+    }
+
+    private void toPushActor(BillingNoticeMain billingNoticeMainClone) {
+        log.info("To PushActor!!");
+        pushMessageRouterActor.tell(billingNoticeMainClone, this.getSelf());
+    }
+
+    private void toExpiredActor(BillingNoticeMain object) {
+        log.info("To ExpireActor!!");
+        expireRouterActor.tell(object, this.getSelf());
+    }
+
+    private void toCurfewActor(BillingNoticeMain object) {
+        log.info("To CurfewActor!!");
+        curfewActor.tell(object, this.getSelf());
+    }
+
+    private void toUpdateActor(Object object) {
+        log.info("To UpdateActor!!");
+        updateStatusRouterActor.tell(object, this.getSelf());
     }
 }
--- com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/service/BillingNoticeAkkaService.java
+++ com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/service/BillingNoticeAkkaService.java
@@ -1,57 +1,53 @@
 package com.bcs.core.taishin.circle.service;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import com.bcs.core.taishin.circle.akka.handler.BillingNoticeMainActor;
+import com.bcs.core.utils.AkkaSystemFactory;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.PreDestroy;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
 
-import javax.annotation.PreDestroy;
-
-import org.apache.log4j.Logger;
-import org.springframework.stereotype.Service;
-
-import com.bcs.core.taishin.circle.akka.handler.BillingNoticeMainActor;
-import com.bcs.core.utils.AkkaSystemFactory;
-
-import akka.actor.ActorRef;
-import akka.actor.ActorSystem;
-
+/**
+ * @author ???
+ */
+@Slf4j
 @Service
 public class BillingNoticeAkkaService {
-	/** Logger */
-	private static Logger logger = Logger.getLogger(BillingNoticeAkkaService.class);
-	private List<ActorSystem> actorSystemList = new ArrayList<ActorSystem>();
-	private List<ActorRef> billingNoticeActorList = new ArrayList<ActorRef>();
-	
-	public BillingNoticeAkkaService() {
-		new AkkaSystemFactory<BillingNoticeMainActor>(actorSystemList, billingNoticeActorList, BillingNoticeMainActor.class, "actorSystemList", "BillingNoticeActorList");
-	}
-	
-	public void tell(Object object) {
-		ActorRef actor = getRandomActor(billingNoticeActorList);
-		actor.tell(object, actor);
-	}
-	
-	private ActorRef getRandomActor(List<ActorRef> actors){
+    private List<ActorSystem> actorSystemList = new ArrayList<>();
+    private List<ActorRef> billingNoticeActorList = new ArrayList<>();
+
+    public BillingNoticeAkkaService() {
+        new AkkaSystemFactory<>(actorSystemList, billingNoticeActorList, BillingNoticeMainActor.class, "actorSystemList", "BillingNoticeActorList");
+    }
+
+    public void tell(Object object) {
+        ActorRef actor = getRandomActor(billingNoticeActorList);
+        actor.tell(object, actor);
+    }
+
+    private ActorRef getRandomActor(List<ActorRef> actors) {
         int index = new Random().nextInt(actors.size());
         return actors.get(index);
-	}
-	
-	@PreDestroy
-	public void shutdownNow(){
-		logger.info("[DESTROY] BillingNotice AkkaService shutdownNow cleaning up...");
-		try {
-			int count = 0;
-			for(ActorSystem system : actorSystemList){
-				system.stop(billingNoticeActorList.get(count));
-				count++;
-				
-				system.shutdown();
-				system = null;
-			}
-		} catch (Throwable e) {
-		}
-		
-		System.gc();
-		logger.info("[DESTROY] BillingNotice AkkaService shutdownNow destroyed");
-	}
+    }
+
+    @PreDestroy
+    public void shutdownNow() {
+        log.info("[DESTROY] BillingNotice AkkaService shutdownNow cleaning up...");
+        try {
+            int count = 0;
+            for (ActorSystem system : actorSystemList) {
+                system.stop(billingNoticeActorList.get(count));
+                count++;
+
+                system.shutdown();
+            }
+        } catch (Throwable e) {
+        }
+        log.info("[DESTROY] BillingNotice AkkaService shutdownNow destroyed");
+    }
 }

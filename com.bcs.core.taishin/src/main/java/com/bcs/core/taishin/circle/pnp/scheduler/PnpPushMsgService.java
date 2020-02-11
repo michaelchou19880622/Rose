b354db025c899a7af94fb3b1c19b9b1e29c08036
Alan Chen <t0239184ps@gmail.com>
2020-02-11 16:16:52 +0800

    [UPDATE] 1. Change production pnp log path and add pnp trace log!!

--- com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/pnp/scheduler/PnpPushMsgService.java
+++ com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/pnp/scheduler/PnpPushMsgService.java
@@ -120,8 +120,13 @@ public class PnpPushMsgService {
                     log.info("Main id and after filter detail list size, main id: {}, after filter detail size: {}", main.getPnpMainId(), filterDetailList.size());
                     log.info("After filter detail list: {}", DataUtils.toPrettyJsonUseJackson(filterDetailList));
                     /* If detail list is empty not include any sending and detail from db is all sent  */
-                    if (filterDetailList.isEmpty() && pnpRepositoryCustom.checkIsAllSent(type, main.getPnpMainId()) == 0) {
+                    boolean notSendIsEmpty = filterDetailList.isEmpty();
+                    boolean lastSendingIsEmpty = pnpRepositoryCustom.checkIsAllSent(type, main.getPnpMainId()) == 0;
+                    log.info("Filter detail list is empty (Wait,Process)->Sending ? {}", notSendIsEmpty);
+                    log.info("Last Sending is empty (equal zero) ? {}", lastSendingIsEmpty);
+                    if (notSendIsEmpty && lastSendingIsEmpty) {
                         /* all detail sent to bc and pnp is finish, Update main status to complete, but not include sms */
+                        log.info("Before update main to complete!!");
                         pnpRepositoryCustom.updateMainToComplete(main.getPnpMainId(), type, PnpStatusEnum.COMPLETE);
                     } else {
                         main.setPnpDetails(filterDetailList);

98949618956fca3555f3aa504083c86e04b2ed28
ChousenChen <chousen@hpicorp.com.tw>
2020-03-05 12:09:08 +0800

    加 log

--- com.bcs.core.bot/src/main/java/com/bcs/core/bot/send/akka/handler/PushMessageActor.java
+++ com.bcs.core.bot/src/main/java/com/bcs/core/bot/send/akka/handler/PushMessageActor.java
@@ -21,33 +21,46 @@ import com.bcs.core.spring.ApplicationContextProvider;
 import com.bcs.core.utils.RestfulUtil;
 
 import akka.actor.UntypedActor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class PushMessageActor extends UntypedActor {
 	@Override
 	public void onReceive(Object object) throws Exception {
+		log.info("---------------- PushMessageActor ----------------");
 		if(object instanceof PushApiModel) {
 			PushApiModel pushApiModel = (PushApiModel) object;
+			log.info("PushApiModel => {}", pushApiModel);
+			log.info("pushApiModel.getSendTimeType() = {}", pushApiModel.getSendTimeType());
 
 			if(pushApiModel.getSendTimeType().equals(PushApiModel.SEND_TYPE_IMMEDIATE)) {	// 立即發送
 				String url = CoreConfigReader.getString(CONFIG_STR.LINE_MESSAGE_PUSH_URL.toString());
+				log.info("URL => {}", url);
+				
 				String accessToken = CoreConfigReader.getString(CONFIG_STR.DEFAULT.toString(), CONFIG_STR.CHANNEL_TOKEN.toString(), true);
+				log.info("accessToken => {}", accessToken);
+				
 				String serviceCode = CoreConfigReader.getString(CONFIG_STR.AUTO_REPLY.toString(), CONFIG_STR.CHANNEL_SERVICE_CODE.toString(), true);
+				log.info("serviceCode => {}", serviceCode);
+				
 				JSONObject requestBody = new JSONObject();
-				PushMessageRecord record = null;
 
 				/* 設定 request headers */
 				HttpHeaders headers = new HttpHeaders();
 				headers.setContentType(MediaType.APPLICATION_JSON_UTF8);
 				headers.set(HttpHeaders.AUTHORIZATION, "Bearer " + accessToken);
 				headers.set(LINE_HEADER.HEADER_BOT_ServiceCode.toString(), serviceCode);
+				log.info("Headers => {}", headers);
 
 				requestBody.put("messages", pushApiModel.getMessages());
 
 				JSONArray uids = pushApiModel.getUid();
+				log.info("Uids => {}", uids);
 				for(Integer i = 0; i < uids.length(); i++) {
-					record = new PushMessageRecord();
+					PushMessageRecord record = new PushMessageRecord();
 
 					requestBody.put("to", uids.get(i));
+					log.info("requestBody => {}", requestBody.toString());
 
 					/* 將 headers 跟 body 塞進 HttpEntity 中  */
 					HttpEntity<String> httpEntity = new HttpEntity<String>(requestBody.toString(), headers);
@@ -55,7 +68,16 @@ public class PushMessageActor extends UntypedActor {
 					RestfulUtil restfulUtil = new RestfulUtil(HttpMethod.POST, url, httpEntity);
 
 					try {
-						restfulUtil.execute();
+						JSONObject jsonObjOfRestfulExcuted = restfulUtil.execute();
+						log.info("jsonObjOfRestfulExcuted => {}", jsonObjOfRestfulExcuted);
+						
+						String sendMessage = pushApiModel.getMessages().toString();
+						log.info("sendMessage => {}", sendMessage);
+						log.info("sendMessage Length => {}", sendMessage.length());
+						
+						if (sendMessage.length() >= 200) {  // 因為DB欄位設定的關係(nvarchar 255)，暫時修改最多只取200長度的data。
+							sendMessage = sendMessage.substring(0, 200);
+						}
 
 						record.setDepartment(pushApiModel.getDepartment());
 						record.setUID(uids.get(i).toString());
@@ -68,7 +90,24 @@ public class PushMessageActor extends UntypedActor {
 						record.setServiceName(pushApiModel.getServiceName());
 						record.setPushTheme(pushApiModel.getPushTheme());
 					} catch (HttpClientErrorException e) {
+						log.info("HttpClientErrorException => {}", e);
 						JSONObject errorMessage = new JSONObject(e.getResponseBodyAsString());
+						
+						String strErrorMessage = errorMessage.getString("message");
+						log.info("strErrorMessage => {}", strErrorMessage);
+						log.info("strErrorMessage Length => {}", strErrorMessage.length());
+						
+						if (strErrorMessage.length() >= 200) {  // 因為DB欄位設定的關係(nvarchar 255)，暫時修改最多只取200長度的data。
+							strErrorMessage = strErrorMessage.substring(0, 200);
+						}
+						
+						String sendMessage = pushApiModel.getMessages().toString();
+						log.info("sendMessage = {}", sendMessage);
+						log.info("sendMessage.length() = {}", sendMessage.length());
+						
+						if (sendMessage.length() >= 200) {  // 因為DB欄位設定的關係(nvarchar 255)，暫時修改最多只取200長度的data。
+							sendMessage = sendMessage.substring(0, 200);
+						}
 
 						if(errorMessage.has("message")) {
 							record.setDepartment(pushApiModel.getDepartment());
@@ -83,11 +122,41 @@ public class PushMessageActor extends UntypedActor {
 							record.setPushTheme(pushApiModel.getPushTheme());
 							if(errorMessage.has("details"))
 								record.setDetailMessage(errorMessage.getJSONArray("details").toString());
+						} 
+					} catch (Exception e) {
+						log.info("Exception = {}", e);
+						
+						String exceptionErrorMessage = e.getMessage();
+						log.info("exceptionErrorMessage = {}", exceptionErrorMessage);
+						log.info("exceptionErrorMessage.length() = {}", exceptionErrorMessage.length());
+						
+						if (exceptionErrorMessage.length() >= 200) {  // 因為DB欄位設定的關係(nvarchar 255)，暫時修改最多只取200長度的data。
+							exceptionErrorMessage = exceptionErrorMessage.substring(0, 200);
 						}
+						
+						String sendMessage = pushApiModel.getMessages().toString();
+						log.info("sendMessage = {}", sendMessage);
+						log.info("sendMessage.length() = {}", sendMessage.length());
+						
+						if (sendMessage.length() >= 200) {  // 因為DB欄位設定的關係(nvarchar 255)，暫時修改最多只取200長度的data。
+							sendMessage = sendMessage.substring(0, 200);
+						}
+						
+						record.setDepartment(pushApiModel.getDepartment());
+						record.setSendTime(new Date());
+						record.setServiceName(pushApiModel.getServiceName());
+						record.setPushTheme(pushApiModel.getPushTheme());
+						record.setUID(uids.get(i).toString());
+						record.setSendMessage(sendMessage);
+						record.setStatusCode("none");
+						record.setMainMessage(exceptionErrorMessage);
+						record.setSendType(pushApiModel.getSendTimeType());
+						record.setCreateTime(pushApiModel.getTriggerTime());
 					}
 					this.getSender().tell(record, this.getSelf());
 				}
 			} else {	// 預約發送
+				log.info("pushApiModel = {}", pushApiModel);
 				PushMessageTaskService pushMessageTaskService = ApplicationContextProvider.getApplicationContext().getBean(PushMessageTaskService.class);
 
 				pushMessageTaskService.startTask(pushApiModel);
--- com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/api/controller/PushApiController.java
+++ com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/api/controller/PushApiController.java
@@ -1,13 +1,13 @@
 package com.bcs.core.taishin.api.controller;
 
-import com.bcs.core.api.service.model.PushApiModel;
-import com.bcs.core.aspect.annotation.WebServiceLog;
-import com.bcs.core.bot.akka.service.PNPService;
-import com.bcs.core.enums.CONFIG_STR;
-import com.bcs.core.resource.CoreConfigReader;
-import com.bcs.core.taishin.api.validator.PushApiRequestValidator;
-import com.bcs.core.utils.CryptUtil;
-import org.apache.log4j.Logger;
+import java.util.Date;
+
+import javax.crypto.BadPaddingException;
+import javax.crypto.IllegalBlockSizeException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.json.JSONArray;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
@@ -18,25 +18,35 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import javax.crypto.BadPaddingException;
-import javax.crypto.IllegalBlockSizeException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import com.bcs.core.api.service.model.PushApiModel;
+import com.bcs.core.aspect.annotation.WebServiceLog;
+import com.bcs.core.bot.akka.service.PNPService;
+import com.bcs.core.db.entity.PushMessageRecord;
+import com.bcs.core.db.service.PushMessageRecordService;
+import com.bcs.core.enums.CONFIG_STR;
+import com.bcs.core.resource.CoreConfigReader;
+import com.bcs.core.spring.ApplicationContextProvider;
+import com.bcs.core.taishin.api.validator.PushApiRequestValidator;
+import com.bcs.core.utils.CryptUtil;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class PushApiController {
-    private static Logger logger = Logger.getLogger(PushApiController.class);
 
     @Autowired
     private PNPService PNPService;
+    
+    private PushApiModel pushApiModel;
 
     @WebServiceLog
     @RequestMapping(method = RequestMethod.POST, value = "/message/push", consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public ResponseEntity<?> pushMessage(HttpServletRequest request, HttpServletResponse response, @RequestBody String requestBodyString) {
         try {
-            logger.info("-------------------- pushMessage --------------------");
-            logger.info("[pushMessage] Request body: " + requestBodyString);
+            log.info("-------------------- pushMessage --------------------");
+            log.info("[pushMessage] Request body: " + requestBodyString);
 
             if (request.getHeader(HttpHeaders.AUTHORIZATION) == null) {
                 return new ResponseEntity<>("{\"result\": 0, \"msg\": \"Missing 'Authorization' header.\"}", HttpStatus.BAD_REQUEST);
@@ -48,33 +58,72 @@ public class PushApiController {
                 }
 
                 String token = authorization.split("key=")[1];
-                logger.info("[pushMessage] token: " + token);
+                log.info("[pushMessage] token: " + token);
                 
                 String secret = CoreConfigReader.getString(CONFIG_STR.AES_SECRET_KEY, true);
-                logger.info("[pushMessage] secret: " + secret);
+                log.info("[pushMessage] secret: " + secret);
                 
                 String iv = CoreConfigReader.getString(CONFIG_STR.AES_INITIALIZATION_VECTOR, true);
-                logger.info("[pushMessage] iv: " + iv);
+                log.info("[pushMessage] iv: " + iv);
                 
                 String originalToken = CoreConfigReader.getString(CONFIG_STR.API_ORIGINAL_TOKEN, true);
-                logger.info("[pushMessage] originalToken: " + originalToken);
+                log.info("[pushMessage] originalToken: " + originalToken);
                 
                 String descryptedToken = CryptUtil.Decrypt(CryptUtil.AES, token, secret, iv);
-                logger.info("[pushMessage] descryptedToken: " + descryptedToken);
+                log.info("[pushMessage] descryptedToken: " + descryptedToken);
 
                 if (!descryptedToken.equals(originalToken)) {
                     return new ResponseEntity<>("{\"result\": 0, \"msg\": \"Invalid token.\"}", HttpStatus.UNAUTHORIZED);
                 }
             }
 
-            PushApiModel pushApiModel = new PushApiModel();
+            pushApiModel = new PushApiModel();
 
             PushApiRequestValidator.validate(requestBodyString, pushApiModel);
+            
+            log.info("pushApiModel => {}", pushApiModel);
 
             PNPService.tell(pushApiModel);
 
             return new ResponseEntity<>("{\"result\": 1, \"msg\": \"Success.\"}", HttpStatus.OK);
         } catch (Exception e) {
+        	JSONArray uids = pushApiModel.getUid();
+        	log.info("uids => {}", uids);
+        	log.info("uids length => {}", uids.length());
+        	
+        	for (Integer i = 0; i < uids.length(); i++) {
+				String sendMessage = pushApiModel.getMessages().toString();
+				log.info("sendMessage = {}", sendMessage);
+				log.info("sendMessage.length() = {}", sendMessage.length());
+				
+				if (sendMessage.length() >= 200) {  // 因為DB欄位設定的關係(nvarchar 255)，暫時修改最多只取200長度的data。
+					sendMessage = sendMessage.substring(0, 200);
+				}
+				
+				// save record
+				PushMessageRecord record = new PushMessageRecord();
+				record.setUID(uids.get(i).toString());
+				record.setSendType(pushApiModel.getSendTimeType());
+				
+				Date dateSendTime = new Date();
+				if (pushApiModel.getSendTimeType().equals(PushApiModel.SEND_TYPE_DELAY)) {
+					dateSendTime = pushApiModel.getSendTimeSet();
+				}
+				record.setDepartment(pushApiModel.getDepartment());
+				record.setServiceName(pushApiModel.getServiceName());
+				record.setPushTheme(pushApiModel.getPushTheme());
+				record.setStatusCode("none");
+				record.setSendTime(dateSendTime);
+				record.setCreateTime(pushApiModel.getTriggerTime());
+				record.setMainMessage("Error");
+				record.setSendMessage(e.getClass().getName() + " occurred, please check the log.");
+				
+				PushMessageRecordService pushMessageRecordService = ApplicationContextProvider.getApplicationContext().getBean(PushMessageRecordService.class);
+				pushMessageRecordService.save(record);
+			}
+
+			log.error("[pushMessage] Exception = {}", e);
+        	
             if (e instanceof IllegalArgumentException)
                 return new ResponseEntity<>("{\"result\": 0, \"msg\": \"" + e.getMessage() + "\"}", HttpStatus.BAD_REQUEST);
             else if (e instanceof BadPaddingException || e instanceof IllegalBlockSizeException || e instanceof IllegalArgumentException)

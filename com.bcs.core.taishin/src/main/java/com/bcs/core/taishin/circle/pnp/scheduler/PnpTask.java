9df83cf2c9a85aab0021ab60fd5b1e3f3591f29f
hongyi <hikuo123@gmail.com>
2020-08-09 14:08:50 +0800

    <優化> PNP各節點逾期不發送(BC、PNP) 1. 新增PNP各節點(BC、PNP) 逾期檢查不發送機制, 超過PNP_DELIVERY_EXPIRY_TIME 120 min即不發送至BC、PNP(假如config讀取有誤, 則default值最少30 min)

--- com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/pnp/scheduler/PnpTask.java
+++ com.bcs.core.taishin/src/main/java/com/bcs/core/taishin/circle/pnp/scheduler/PnpTask.java
@@ -2,12 +2,22 @@ package com.bcs.core.taishin.circle.pnp.scheduler;
 
 import com.bcs.core.spring.ApplicationContextProvider;
 import com.bcs.core.taishin.circle.pnp.code.PnpStageEnum;
+import com.bcs.core.taishin.circle.pnp.code.PnpStatusEnum;
+import com.bcs.core.taishin.circle.pnp.db.entity.AbstractPnpMainEntity;
 import com.bcs.core.taishin.circle.pnp.db.entity.PnpMain;
 import com.bcs.core.taishin.circle.pnp.service.PnpService;
+import com.bcs.core.resource.CoreConfigReader;
+import com.bcs.core.enums.CONFIG_STR;
+import com.bcs.core.utils.DataUtils;
 import lombok.extern.slf4j.Slf4j;
+
 import org.quartz.Job;
 import org.quartz.JobExecutionContext;
 import org.quartz.SchedulerException;
+
+import java.util.Date;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 /**
  * @author ???
@@ -27,6 +37,16 @@ public class PnpTask implements Job {
         try {
             PnpMain pnpMain = (PnpMain) context.getScheduler().getContext().get("PnpMain");
 
+            int expiredTime = CoreConfigReader.getInteger(CONFIG_STR.PNP_DELIVERY_EXPIRED_TIME);
+            // Default : 預設 PNP_DELIVERY_EXPIRE_TIME的timeout的時間最少需30分鐘, 目前系統設定值為120分鐘.
+            if (expiredTime < 30) {
+            	expiredTime = 30;
+            }            
+            Calendar expiredCalendar = new GregorianCalendar();
+            Date scheduleTime = pnpService.getFormatScheduleTimeBySourceSystem(pnpMain.getSource(), pnpMain.getScheduleTime());            
+        	expiredCalendar.setTime(scheduleTime);
+        	expiredCalendar.add(Calendar.MINUTE, expiredTime);            
+            
             /* 取得訊息發送方式 */
             String processStage = pnpMain.getProcStage();
 
@@ -34,10 +54,26 @@ public class PnpTask implements Job {
             PnpStageEnum stage = PnpStageEnum.findEnumByName(processStage);
             switch (stage) {
                 case BC:
-                    pnpService.pushLineMessage(pnpMain, null, null, stage);
+    	            if (DataUtils.inBetween(new Date(), scheduleTime, expiredCalendar.getTime())) {
+    	                log.info("Send a BC Schedule Push Message");
+    	            	pnpService.pushLineMessage(pnpMain, null, null, stage);
+    	            }
+    	            else {
+    	                /* 將 Main/Detail 訊息狀態更改為EXPIRED */
+    	                log.info("Scheduled an expired BC schedule Push Message, update status to [EXPIRED]. ExpiredTime is : {}", expiredCalendar.getTime());	            		            		            	    	            	    	            	
+    	                pnpService.updateMainAndDetailStatus(pnpMain, PnpStatusEnum.EXPIRED.value);            	            	
+    	            }     	        	
                     break;
                 case PNP:
-                    pnpService.pushPnpMessage(pnpMain, null, null, stage);
+    	            if (DataUtils.inBetween(new Date(), scheduleTime, expiredCalendar.getTime())) {
+    	                log.info("Send a PNP Schedule Push Message");    	            	
+    	            	pnpService.pushPnpMessage(pnpMain, null, null, stage);
+    	            }
+    	            else {
+    	                /* 將 Main/Detail 訊息狀態更改為EXPIRED */
+    	                log.info("Scheduled an expired PNP schedule Push Message, update status to [EXPIRED]. ExpiredTime is : {}", expiredCalendar.getTime());	            		            		            	    	            	
+    	                pnpService.updateMainAndDetailStatus(pnpMain, PnpStatusEnum.EXPIRED.value);            	            	    	            	
+    	            }
                     break;
                 default:
                     break;
